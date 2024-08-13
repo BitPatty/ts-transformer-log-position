7a1e9e23ba9b3dd7c927c2cc76b7cfc575517280
Matteias Collet <matteias.collet@protonmail.ch>
2024-08-13 18:54:40 +0000

    update build command

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,17 @@
-import pkg from './package.json' assert { type: 'json' };
+import { readFileSync } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 import typescript from 'rollup-plugin-typescript2';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const packageJson = JSON.parse(readFileSync(join(__dirname, 'package.json')));
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: pkg.main,
+      file: packageJson.main,
       format: 'cjs',
       exports: 'named',
       sourcemap: true,
