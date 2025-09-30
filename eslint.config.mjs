14c252802f446c9689e263138564f2e2ecab9129
Matteias Collet <matteias.collet@protonmail.ch>
2025-09-30 17:24:05 +0000

    update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,107 +1,122 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import jest from "eslint-plugin-jest";
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import jest from 'eslint-plugin-jest';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
+export default [
+  {
     ignores: [
-        "coverage/**/*",
-        "dist/**/*",
-        "scripts/**/*",
-        "**/*.sh",
-        "coverage/**/*",
-        "dist/**/*",
-        "scripts/**/*",
-        "demo/**/*",
-        "**/*.sh",
-        "**/rollup.config.mjs",
-        "test/**/*",
-        "test-output/**/*",
+      'coverage/**/*',
+      'dist/**/*',
+      'scripts/**/*',
+      '**/*.sh',
+      'coverage/**/*',
+      'dist/**/*',
+      'scripts/**/*',
+      'demo/**/*',
+      '**/*.sh',
+      '**/rollup.config.mjs',
+      'test/**/*',
+      'test-output/**/*',
     ],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest/recommended",
-    "plugin:prettier/recommended",
-), {
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        jest,
-        prettier,
+      '@typescript-eslint': typescriptEslint,
+      jest,
+      prettier,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node,
+      },
 
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "commonjs",
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'commonjs',
 
-        parserOptions: {
-            project: "./tsconfig.json",
-            tsconfigRootDir: "./",
-        },
+      parserOptions: {
+        project: './tsconfig.json',
+      },
     },
 
     rules: {
-        "@typescript-eslint/explicit-function-return-type": ["error", {
-            allowExpressions: true,
-            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-        }],
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true,
+          allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+        },
+      ],
 
-        "@typescript-eslint/no-floating-promises": ["error"],
-        "@typescript-eslint/no-shadow": ["error"],
-        "@typescript-eslint/explicit-member-accessibility": ["error"],
-        "@typescript-eslint/no-unused-vars": ["error"],
-        "@typescript-eslint/switch-exhaustiveness-check": "error",
-        "no-console": ["error"],
-        "no-return-await": ["error"],
-        "require-await": ["error"],
+      '@typescript-eslint/no-floating-promises': ['error'],
+      '@typescript-eslint/no-shadow': ['error'],
+      '@typescript-eslint/explicit-member-accessibility': ['error'],
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/switch-exhaustiveness-check': 'error',
+      'no-console': ['error'],
+      'no-return-await': ['error'],
+      'require-await': ['error'],
 
-        "padding-line-between-statements": ["error", {
-            blankLine: "always",
-            prev: "*",
-            next: "function",
-        }],
+      'padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: 'function',
+        },
+      ],
 
-        "prettier/prettier": ["error", {}, {
-            usePrettierrc: true,
-        }],
+      'prettier/prettier': [
+        'error',
+        {},
+        {
+          usePrettierrc: true,
+        },
+      ],
 
-        // "valid-jsdoc": ["error", {
-        //     requireReturn: false,
-        //     requireReturnType: false,
-        //     requireParamType: false,
-        // }],
+      // "valid-jsdoc": ["error", {
+      //     requireReturn: false,
+      //     requireReturnType: false,
+      //     requireParamType: false,
+      // }],
     },
-}, {
-    files: ["**/*.spec.ts"],
+  },
+  {
+    files: ['**/*.spec.ts'],
 
     languageOptions: {
-        globals: {
-            ...jest.environments.globals.globals,
-        },
+      globals: {
+        ...jest.environments.globals.globals,
+      },
     },
-}, {
-    files: ["test/**/*.js"],
+  },
+  {
+    files: ['test/**/*.js'],
 
     rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
+      '@typescript-eslint/explicit-function-return-type': 'off',
     },
-}];
+  },
+];
